cc521477482c2f672d0b2d661dd02b11426cd309
Aravind N <aravindn@Aravinds-MacBook-Air.local>
2026-06-27 10:53:14 +0530

    initial commit

---
+++ src/main/java/com/aravind/aiconnector/AiconnectorApplication.java
@@ -0,0 +1,13 @@
+package com.aravind.aiconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiconnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aravind/aiconnector/AiconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravind.aiconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
